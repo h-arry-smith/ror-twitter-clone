7542c6bb7ff8e038f3d8220549652def0034359c
Harry Smith <harry@hsmith.net>
2022-08-10 13:53:58 +0100

    Basic Devise Set Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  devise_for :users
   # root "articles#index"
+  root "feed#index"
 end
