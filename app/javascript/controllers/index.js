4a7d82eafee98b388afd98ecc2d44c130bf3849a
Harry Smith <harry@hsmith.net>
2022-08-11 11:08:34 +0100

    Live updating bio length tracking and save disable

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import SettingsController from "./settings_controller"
+application.register("settings", SettingsController)
